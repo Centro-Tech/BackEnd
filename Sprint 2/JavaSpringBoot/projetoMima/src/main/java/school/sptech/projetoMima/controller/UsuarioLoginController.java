8c1457fa1566a85d773e3f1a6c3ada13bb8c0e20
JVictorDZ <joao.silva998@sptech.school>
2025-04-27 10:56:24 -0300

    Fix: Correção de UsuarioLoginController

--- Sprint 2/JavaSpringBoot/projetoMima/src/main/java/school/sptech/projetoMima/controller/UsuarioLoginController.java
+++ Sprint 2/JavaSpringBoot/projetoMima/src/main/java/school/sptech/projetoMima/controller/UsuarioLoginController.java
@@ -11,42 +11,26 @@ import school.sptech.projetoMima.entity.Usuario;
 import school.sptech.projetoMima.service.UsuarioLoginService;
 
 import java.util.List;
-
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/login")
 public class UsuarioLoginController {
 
     @Autowired
     private UsuarioLoginService usuarioLoginService;
 
-    @PostMapping
+    @PostMapping("/criar")
     @SecurityRequirement(name = "Bearer")
     public ResponseEntity<Void> criar(@RequestBody @Valid UsuarioCriacaoDto usuarioCriacaoDto) {
-
         final Usuario novoUsuario = UsuarioMapper.of(usuarioCriacaoDto);
         this.usuarioLoginService.criar(novoUsuario);
         return ResponseEntity.status(201).build();
     }
 
-    @PostMapping("/login")
+    @PostMapping("/autenticar")
     public ResponseEntity<UsuarioTokenDto> login(@RequestBody UsuarioLoginDto usuarioLoginDto) {
-
         final Usuario usuario = UsuarioMapper.of(usuarioLoginDto);
         UsuarioTokenDto usuarioTokenDto = this.usuarioLoginService.autenticar(usuario);
 
         return ResponseEntity.status(200).body(usuarioTokenDto);
-    }
-
-    @GetMapping
-    @SecurityRequirement(name = "Bearer")
-    public ResponseEntity<List<UsuarioListarDto>> listarTodos() {
-
-        List<UsuarioListarDto> usuariosEncontrados = this.usuarioLoginService.listarTodos();
-
-        if (usuariosEncontrados.isEmpty()){
-            return ResponseEntity.status(204).build();
-        }
-        return ResponseEntity.status(200).body(usuariosEncontrados);
-
     }
 }

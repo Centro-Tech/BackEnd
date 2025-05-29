fa3216c1940d116e7bae3eded383942698f58eb6
JVictorDZ <joao.silva998@sptech.school>
2025-05-29 17:49:12 -0300

    Feature: Adição de Documentação Swagger em DTOs de auxiliares

--- Sprint 2/JavaSpringBoot/projetoMima/src/main/java/school/sptech/projetoMima/dto/itemDto/auxiliares/CorDto.java
+++ Sprint 2/JavaSpringBoot/projetoMima/src/main/java/school/sptech/projetoMima/dto/itemDto/auxiliares/CorDto.java
@@ -1,33 +1,35 @@
 package school.sptech.projetoMima.dto.itemDto.auxiliares;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import school.sptech.projetoMima.entity.item.Cor;
 
+@Schema(description = "DTO para cor do item")
 public class CorDto {
+
+    @Schema(description = "Nome da cor", example = "Azul", required = true)
     @NotBlank
     @NotNull
     @Size(min = 2)
     private String nome;
 
-    @JsonCreator
+    public CorDto() {}
+
     public CorDto(String nome) {
         this.nome = nome;
     }
 
-    @JsonValue
-    public @NotBlank @NotNull @Size(min = 2) String getNome() {
+    public String getNome() {
         return nome;
     }
 
-    public void setNome(@NotBlank @NotNull @Size(min = 2) String nome) {
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public static Cor toEntity (CorDto cor) {
+    public static Cor toEntity(CorDto cor) {
         Cor response = new Cor();
         response.setNome(cor.getNome());
         return response;
--- Sprint 2/JavaSpringBoot/projetoMima/src/main/java/school/sptech/projetoMima/dto/itemDto/auxiliares/MaterialDto.java
+++ Sprint 2/JavaSpringBoot/projetoMima/src/main/java/school/sptech/projetoMima/dto/itemDto/auxiliares/MaterialDto.java
@@ -1,29 +1,31 @@
 package school.sptech.projetoMima.dto.itemDto.auxiliares;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import school.sptech.projetoMima.entity.item.Material;
 
+@Schema(description = "DTO para material do item")
 public class MaterialDto {
+
+    @Schema(description = "Nome do material", example = "Algodão", required = true)
     @NotBlank
     @NotNull
     @Size(min = 2)
     private String material;
 
-    @JsonCreator
+    public MaterialDto() {}
+
     public MaterialDto(String material) {
         this.material = material;
     }
 
-    @JsonValue
-    public @NotBlank @NotNull @Size(min = 2) String getMaterial() {
+    public String getMaterial() {
         return material;
     }
 
-    public void setMaterial(@NotBlank @NotNull @Size(min = 2) String material) {
+    public void setMaterial(String material) {
         this.material = material;
     }
 

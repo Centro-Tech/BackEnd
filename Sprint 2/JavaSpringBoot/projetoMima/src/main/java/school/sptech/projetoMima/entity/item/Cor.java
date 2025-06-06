dd9e4955e20114be6701b25901772c44e91faf1a
JVictorDZ <joao.silva998@sptech.school>
2025-06-05 22:32:27 -0300

    Feature: Swagger nas entidades auxiliares

--- Sprint 2/JavaSpringBoot/projetoMima/src/main/java/school/sptech/projetoMima/entity/item/Cor.java
+++ Sprint 2/JavaSpringBoot/projetoMima/src/main/java/school/sptech/projetoMima/entity/item/Cor.java
@@ -1,15 +1,20 @@
 package school.sptech.projetoMima.entity.item;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
+@Schema(name = "Cor", description = "Representa a cor de um item")
 public class Cor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Identificador da cor", example = "1")
     private Integer id;
+
+    @Schema(description = "Nome da cor", example = "Azul")
     private String nome;
 
     public Integer getId() {
--- Sprint 2/JavaSpringBoot/projetoMima/src/main/java/school/sptech/projetoMima/entity/item/Material.java
+++ Sprint 2/JavaSpringBoot/projetoMima/src/main/java/school/sptech/projetoMima/entity/item/Material.java
@@ -1,15 +1,20 @@
 package school.sptech.projetoMima.entity.item;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
+@Schema(name = "Material", description = "Representa o material de um item")
 public class Material {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Identificador do material", example = "1")
     private Integer id;
+
+    @Schema(description = "Nome do material", example = "Algodão")
     private String nome;
 
     public Integer getId() {
@@ -27,5 +32,4 @@ public class Material {
     public void setNome(String nome) {
         this.nome = nome;
     }
-
 }
